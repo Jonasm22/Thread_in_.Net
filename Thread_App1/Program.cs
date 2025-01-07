05386ce95be30d26e782f6f856afd31a52960ea3
Jonathan Mendoza <john.2114@hotmail.com>
2025-01-07 14:33:14 +0100

    New Lectures Added

---
+++ Thread_App1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Thread_App1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Thread_App2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Thread_App2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var taskCompletionSource = new TaskCompletionSource<bool>();
+
+        var thread = new Thread(() =>
+        {
+            Console.WriteLine($"Thread number:  {Thread.CurrentThread.ManagedThreadId} Started");
+            Thread.Sleep(5000);
+            taskCompletionSource.TrySetResult(true);
+            Console.WriteLine($"Thread number:  {Thread.CurrentThread.ManagedThreadId} Completed");
+        });
+        
+       thread.Start();
+       var test = taskCompletionSource.Task.Result;
+       Console.WriteLine($"Thread number:  {test} was done");
+       Console.ReadLine();
+
+    }
+}
